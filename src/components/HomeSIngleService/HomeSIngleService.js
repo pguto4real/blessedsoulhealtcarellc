54893bbdda7fca15cc20254cf36ca2bc0cea101b
oluwamuibiclear <igbokweudoka@gmail.com>
2025-01-12 22:16:47 -0600

    the blocks should be fixed to all be the same size.

--- src/components/HomeSIngleService/HomeSIngleService.js
+++ src/components/HomeSIngleService/HomeSIngleService.js
@@ -5,17 +5,24 @@ const HomeSIngleService = ({ icon, title, border_class,sub}) => {
    return (
       <>
          <div className="col-xl-4 col-lg-6 col-md-6">
-            <div className={border_class ? `service-box ${border_class} text-center mb-30` : 'service-box text-center mb-30'}>
-               <div className="service-thumb">
-                  <img src={icon} alt="" style={{ width: "122px", height: "120px"}}/>
-               </div>
-               <div className="service-content">
-                  <h3>{title}</h3>
-                  <p>{sub}</p>
-                  {/* <Link className="service-link" to="/servicesDetails">Read More</Link> */}
-               </div>
-            </div>
-         </div>
+  <div
+    className={
+      border_class
+        ? `service-box ${border_class} text-center mb-30`
+        : 'service-box text-center mb-30'
+    }
+    style={{ height: '400px' }} // Set a fixed height here
+  >
+    <div className="service-thumb">
+      <img src={icon} alt="" style={{ width: '122px', height: '120px' }} />
+    </div>
+    <div className="service-content">
+      <h3>{title}</h3>
+      <p>{sub}</p>
+      {/* <Link className="service-link" to="/servicesDetails">Read More</Link> */}
+    </div>
+  </div>
+</div>
       </>
    );
 };
